49479891cea55abda387ced362b2e713eda4cb56
Maxim Trusakov <ultion2010@gmail.com>
2022-05-15 23:08:04 +0400

    nitial commit

---
+++ 3D Person Test/Assets/ShowFPS.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowFPS : MonoBehaviour
+{
+    public Text TextFPS;
+    private float timeCount = 0.0f;
+    private int fpsCount = 0;
+
+
+    void Update()
+    {
+        if (timeCount >= 1)
+        {
+            if (TextFPS != null)
+                TextFPS.text = $"FPS: {fpsCount}";
+            timeCount = 0.0f;
+            fpsCount = 0;
+        }
+        timeCount += Time.deltaTime;
+        fpsCount += 1;
+    }
+}
